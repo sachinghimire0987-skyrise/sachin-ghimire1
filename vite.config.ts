8c277681f2cb3768e7cc1615fc37319dcdffe5c6
sachinghimire0987-skyrise <sachin.ghimire0987@gmail.com>
2026-08-14 22:31:46 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
