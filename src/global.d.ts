ace40a60d5a579ab141a5ac0b4d59689ea003638
sachinghimire0987-skyrise <sachin.ghimire0987@gmail.com>
2026-08-15 12:40:43 +0545

    Refactor CSS and SCSS type declarations
    Refactor type declarations for CSS and SCSS imports to use a consistent module structure.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,9 @@
-// Type declarations for importing CSS and related style files in TypeScript
-// This lets TypeScript accept side-effect imports like `import './App.css'`.
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
 
-declare module '*.css';
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.less';
-
-declare module '*.module.css';
-declare module '*.module.scss';
-declare module '*.module.sass';
-declare module '*.module.less';
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
