58da396bcb30e412b00269020b074e9a72d22b3f
sachinghimire0987-skyrise <sachin.ghimire0987@gmail.com>
2026-08-15 12:46:05 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: false, // Yo line thapda prepareOutDir ko error aaudaina
   },
 });
